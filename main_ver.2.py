f42bac1f5d667e152c7799769abaf6463188b824
watanabe ryunosuke <46173232+berry8848@users.noreply.github.com>
2023-05-30 12:53:23 +0900

    Update main_ver.2.py

--- main_ver.2.py
+++ main_ver.2.py
@@ -6,72 +6,35 @@ from modules import Point
 from modules import Gauss
 from modules import Biharmonic
 from modules import CrossingNumberAlgorithm
+from modules import CheckDistance
 
 import numpy as np
 import csv
-import math
 import time
 import random
 
 # define
 MAXIMUM_NUMBER_OF_SEARCHES = 800 # 点が連続でN回生成できなかったら終了
-MAXIMUM_NUMBER_OF_POINTS = 20 # 最大生成点数
-COEFFICIENT_OF_LONG = 10 # 点間距離に掛ける係数
+MAXIMUM_NUMBER_OF_POINTS = 500 # 最大生成点数
+COEFFICIENT_OF_LONG = 7 # 点間距離に掛ける係数
+SPLIT = 3313 # CNAで用いる．vertexとfaceの分け目．
 
 # Inputファイル
-file_name_2 = './Input/Column10_0615.csv' # ANSYSのデータファイル
-# file_name_2 = './Input/square1220.csv' # ANSYSのデータファイル\
-# file_name_2 = './Input/joint.csv' # ANSYSのデータファイル
+input_path = './Input/Column10_0615.csv' # ANSYSのデータファイル
+# input_path = './Input/square1220.csv' # ANSYSのデータファイル\
+# input_path = './Input/joint.csv' # ANSYSのデータファイル
 
 # Outputファイル
-file_resultPly = 'Output/result_main/result.ply'
-file_resultCsv = 'Output/result_main/result.csv'
-
-
-# 応力と密度の関係式．PDSのみに使用　※関係式が微妙なため，臨時で別の関数
-def stress_to_density(stress):
-    if stress >= 0: #生データが正の場合，0を返す．（通常はマイナスの値をとる）
-        density = 1.448
-    else:
-        #density = -stress/15
-        density = 3*0.00001*(stress)*(stress) + 0.01*(stress) + 1.448
-    return density
-
-# 密度と点間距離の関係式
-def density_to_long(density):
-    long = 6 * 0.1 * math.sqrt(math.sqrt(2)*math.pi/ density)
-    return COEFFICIENT_OF_LONG*long
-
-# 点間距離内に他の点が含まれているか否かを判定．
-# 点間距離内に他の点が含まれていたらFalseを返す．
-def check_distance(fixed_points, candidate_point, long):
-    b_x = candidate_point.x 
-    b_y = candidate_point.y
-    b_z = candidate_point.z
-    check = True
-    for a in fixed_points:
-        a_x = a[0]
-        a_y = a[1]
-        a_z = a[2]
-        distance = (a_x- b_x)**2 + (a_y- b_y)**2 + (a_z- b_z)**2
-        if long**2 > distance:
-            check = False
-            break
-    return check
+result_ply_path = 'Output/result_main/result.ply'
+result_csv_path = 'Output/result_main/result.csv'
 
 
 def main():
-    a2 = [] #file2の入力用
-    points_obj_list = []  #Pointのオブジェクトを保持。
-
-    # ファイル2読み込み
-    with open(file_name_2) as f2:
-        reader2 = csv.reader(f2)
-        for row in reader2:
-            a2.append(row) 
+    points_obj_list = []  # Pointオブジェクトを保持
+    fixed_points = [] # 確定点格納用
 
     # ANSYS上の点群を取得し座標値を取得
-    points = np.loadtxt(file_name_2, delimiter=',')
+    points = np.loadtxt(input_path, delimiter=',')
     print('points = ',points[0:3])
 
 
@@ -83,12 +46,11 @@ def main():
 
     # Gauss
     gauss = Gauss.Gauss(points)
-    _, lambdas , cs= gauss.gauss1()
+    _, lambdas , cs= gauss.gauss()
 
     # Biharmonic
     biharmonic = Biharmonic.Biharmonic(points, cs, lambdas)
 
-    # 以下，PDS
     # PDSでの点の生成範囲の設定
     x_max = points[0][1]
     x_min = points[0][1]
@@ -118,10 +80,12 @@ def main():
     print("y_max = ", y_max, "y_min = ", y_min)
     print("z_max = ", z_max, "z_min = ", z_min)
 
-    # CrossNumberAlgorithm
-    CNA = CrossingNumberAlgorithm.CrossingNumberAlgorithm()
+    # 交差数判定法
+    CNA = CrossingNumberAlgorithm.CrossingNumberAlgorithm(SPLIT)
 
-    fixed_points = [] # 確定点格納用
+    # PDS用
+    CD = CheckDistance.CheckDistance(COEFFICIENT_OF_LONG)
+
     num = 0
     while num < MAXIMUM_NUMBER_OF_SEARCHES:
         if len(fixed_points) >= MAXIMUM_NUMBER_OF_POINTS:
@@ -138,18 +102,13 @@ def main():
             # 物体内部の点か判定
             flg_P = CNA.cramer(pds_point)
 
-
         # 生成点の座標情報をPointに格納し候補点にする
         candidate_point = Point.Point(pds_point)
         candidate_point.pds_coordinate()
         # Biharmonicより候補点の応力を導出
         new_stress = biharmonic.cal(candidate_point.x, candidate_point.y, candidate_point.z)
-        # 導出した応力より候補点の満たすべき密度を導出
-        density = stress_to_density(new_stress)
-        # 導出した密度より候補点の満たすべき点間距離を導出
-        long = density_to_long(density)
-        # 点間距離内に他の点が存在するかを確認
-        flg = check_distance(fixed_points, candidate_point, long)
+        
+        flg = CD.check_distance(fixed_points, candidate_point, new_stress)
 
         # 点間距離内に他の点が存在しないとき候補点を確定点に追加
         if flg:
@@ -161,19 +120,12 @@ def main():
             num = num + 1
             print('num : ', num)
 
-
-    #物体表面上でPDS
-    # CNA.biharmonic(points, cs, lambdas)
-    # CNA.surface_pds(fixed_points)
-    # CNA.surface(fixed_points)
-    CNA.surface_kikalab(fixed_points)
-
     #重複した座標を削除
     fixed_points = np.unique(fixed_points, axis=0)
 
     # ply にPDSの結果出力
     print("fixed_points  = ", len(fixed_points), "個")
-    with open(file_resultPly, 'w', newline="") as f:
+    with open(result_ply_path, 'w', newline="") as f:
         f.write('ply\n')
         f.write('format ascii 1.0\n')
         f.write('element vertex '+str(len(fixed_points))+'\n')
@@ -185,12 +137,10 @@ def main():
             f.write(str(ele[0])+' '+str(ele[1])+' '+str(ele[2])+' '+'\n')
 
     # csv にPDSの結果出力
-    with open(file_resultCsv, 'w', newline="") as f:
+    with open(result_csv_path, 'w', newline="") as f:
         writer = csv.writer(f)
         writer.writerows(fixed_points)
     
-
-
 
 
 if __name__ == '__main__':
