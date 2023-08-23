6886870acbfbc694334542520cfe527649ec6996
watanabe ryunosuke <46173232+berry8848@users.noreply.github.com>
2023-08-23 19:14:40 +0900

    Update CrossingNumberAlgorithm.py

--- modules/CrossingNumberAlgorithm.py
+++ modules/CrossingNumberAlgorithm.py
@@ -1,38 +1,34 @@
 import numpy as np
 import random
 import math
+import sys
+import trimesh
 
 #交差数判定法
 class CrossingNumberAlgorithm:
-    def __init__(self, SPLIT, mesh_data):
+    def __init__(self, mesh_data):
         #self.origin = np.array([(x_max+x_min)/2, (y_max+y_min)/2, (z_max+z_min)/2])
         # 表面形状データの読み込み
         print("読み込み中...")
-        f = open(mesh_data)
-        self.list1 = []
-        self.list2 = []
-        count = 1
-        for line in f.readlines():
-            # vertexとfaceに分けてlist化
-            if count < SPLIT: # vertex 形状データによって値を変更
-                self.list1.append(line.split())
-            else: # list
-                self.list2.append(line.split())
-            count+=1
+        # 3Dメッシュデータを読み込む
+        mesh = trimesh.load(mesh_data)
+        self.list1 = mesh.vertices
+        self.list2 = mesh.faces
         # strをfloatに変換．list1はメートル表記からmm表記に変換
         self.list1 = [[float(x) * 1000 for x in y] for y in self.list1]
-        self.list2 = [[int(x) for x in y] for y in self.list2]
-        print('list1 = ',self.list1[:5])
-        print('list2 = ',self.list2[:5])
-        print("読み込み終了")
+        # self.list2 = [[int(x) for x in y] for y in self.list2]
+        # print('list1 = ',self.list1[:5])
+        # print('list2 = ',self.list2[:5])
 
         #surface用にnp変換
         self.np_list1 = np.array(self.list1)
         self.np_list2 = np.array(self.list2)
-        self.np_list1 = self.np_list1[:,0:3] #[x, y, z]座標
-        self.np_list2 = self.np_list2[:,1:4] #[点番号１, 点番号2, 点番号3] ex[0, 5, 2]
-        # print('np_list1 = ',self.np_list1[:5])
-        # print('np_list2 = ',self.np_list2[:5])    
+        # self.np_list1 = self.np_list1[:,0:3] #[x, y, z]座標
+        # self.np_list2 = self.np_list2[:,0:3] #[点番号１, 点番号2, 点番号3] ex[0, 5, 2]
+        print('np_list1 = ',self.np_list1[:5])
+        print('np_list2 = ',self.np_list2[:5])    
+        print('len(np_list1) = ', len(self.np_list1))
+        print("読み込み終了")
 
 
     #物体表面上に点を生成（キカラボさん手法）       
@@ -83,26 +79,40 @@ class CrossingNumberAlgorithm:
 
     def cramer(self, pds_point): #クラメルの公式を用いて内外判定
         #ray = self.origin - pds_point
-        ray = np.array([10, 10, 10]) - pds_point
+        ray = [np.array([0.1, 10, 10]) - pds_point, 
+               np.array([10, 10, 1]) - pds_point,
+               np.array([10, 1, 10]) - pds_point]
         cross_num = 0
+        edge_count = 0
         for l in self.list2:
-            v0 = np.array([self.list1[l[1]][0], self.list1[l[1]][1], self.list1[l[1]][2]]) # [v0_x, v0_y, v0_z]の順
-            v1 = np.array([self.list1[l[2]][0], self.list1[l[2]][1], self.list1[l[2]][2]])
-            v2 = np.array([self.list1[l[3]][0], self.list1[l[3]][1], self.list1[l[3]][2]])
+            v0 = np.array([self.list1[l[0]][0], self.list1[l[0]][1], self.list1[l[0]][2]]) # [v0_x, v0_y, v0_z]の順
+            v1 = np.array([self.list1[l[1]][0], self.list1[l[1]][1], self.list1[l[1]][2]])
+            v2 = np.array([self.list1[l[2]][0], self.list1[l[2]][1], self.list1[l[2]][2]])
 
             OA = v1 - v0
             OB = v2 - v0
-
-            left = [[OA[0], OB[0], ray[0]], 
-                    [OA[1], OB[1], ray[1]], 
-                    [OA[2], OB[2], ray[2]]]
-            
             right = pds_point - v0
+            left = [[OA[0], OB[0], ray[0][0]], 
+                    [OA[1], OB[1], ray[0][1]], 
+                    [OA[2], OB[2], ray[0][2]]]
 
-            u, v, t = np.linalg.solve(left, right)
+            solution = np.linalg.solve(left, right)
+            #solution = gaussian_elimination_3d(OA, OB, ray[0], right)
+
+            # 解が存在するか否かを判断
+            if solution is not None:
+                u, v, t = solution
+                #print(f"The solution is: x = {u}, y = {v}, z = {t}")
+            else:
+                print("The system of equations has no unique solution.")
+                continue
             #print('u, v, t = ', u, v, t)
 
-            if t>=0 and u>=0 and u<=1 and v>=0 and v<=1 and u+v>=0 and u+v<=1:
+            if t<=0 and u>=0 and u<=1 and v>=0 and v<=1 and u+v>=0 and u+v<=1:
+                if u == 0 or v == 0 or u+v == 1:
+                    print('境界線上に交点が存在します')
+                    print('(u, v, u+v) = ', u, v, u+v)
+                    edge_count += 1
                 cross_num += 1 #三角形の平面内で交点を持つ → カウント
                 #print('u, v, t = ', u, v, t)
                 #print('{:.1000f}'.format(u))
@@ -110,13 +120,33 @@ class CrossingNumberAlgorithm:
             else:
                 continue #三角形の平面外で交点を持つ
         
+        if edge_count != 0: print('edge_count = ', edge_count)        
         #print('cross_num', cross_num)
         if cross_num % 2 == 0:
             flg = False #外側
+            print('PDS_point ：', pds_point)
         else:
             flg = True #内側
-        #print('内外判定結果：', flg)
+        print('内外判定結果：', flg)
         return flg
+    
+
+    def is_point_inside_mesh(point, mesh_vertices):
+        def sign(p1, p2, p3):
+            return (p1[0] - p3[0]) * (p2[1] - p3[1]) - (p2[0] - p3[0]) * (p1[1] - p3[1])
+
+        def point_in_triangle(pt, v1, v2, v3):
+            b1 = sign(pt, v1, v2) < 0.0
+            b2 = sign(pt, v2, v3) < 0.0
+            b3 = sign(pt, v3, v1) < 0.0
+            return b1 == b2 == b3
+
+        for i in range(0, len(mesh_vertices), 3):
+            v1, v2, v3 = mesh_vertices[i], mesh_vertices[i + 1], mesh_vertices[i + 2]
+            if point_in_triangle(point, v1, v2, v3):
+                return True
+
+        return False
 
 
 # 応力と密度の関係式．PDSのみに使用　※関係式が微妙なため，臨時で別の関数
@@ -213,3 +243,46 @@ def thinning_pds(points, PDS_PITCH):
     print('間引き前表面生成点(重複削除済み): ', i)
 
     return fixed_points
+
+
+
+def gaussian_elimination_3d(a, b, c, d):
+    # 拡大係数行列を作成
+    augmented_matrix = [
+        [a[0], b[0], c[0], d[0]],
+        [a[1], b[1], c[1], d[1]],
+        [a[2], b[2], c[2], d[2]]
+    ]
+
+    # 上三角行列に変換
+    for i in range(3):
+        # ピボット選択
+        pivot_row = i
+        for j in range(i + 1, 3):
+            if abs(augmented_matrix[j][i]) > abs(augmented_matrix[pivot_row][i]):
+                pivot_row = j
+        augmented_matrix[i], augmented_matrix[pivot_row] = augmented_matrix[pivot_row], augmented_matrix[i]
+
+        # ピボットが0の場合、一意の解を持たない
+        if augmented_matrix[i][i] == 0:
+            return None
+
+        # ピボットの行を正規化
+        pivot_value = augmented_matrix[i][i]
+        for j in range(i, 4):
+            augmented_matrix[i][j] /= pivot_value
+
+        # ピボットの列を0にする
+        for j in range(i + 1, 3):
+            factor = augmented_matrix[j][i]
+            for k in range(i, 4):
+                augmented_matrix[j][k] -= factor * augmented_matrix[i][k]
+
+    # 後退代入
+    solution = [0, 0, 0]
+    for i in range(2, -1, -1):
+        solution[i] = augmented_matrix[i][3]
+        for j in range(i + 1, 3):
+            solution[i] -= augmented_matrix[i][j] * solution[j]
+
+    return tuple(solution)
